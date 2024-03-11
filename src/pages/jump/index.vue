af8cde117549bb680826d9c6ddaf84524d9e9b78
Justin Birdsall <Birdsaju@mail.gvsu.edu>
2024-03-11 07:44:28 -0400

    test

--- src/pages/jump/index.vue
+++ src/pages/jump/index.vue
@@ -43,7 +43,7 @@ onMounted(() => {
                 </div>
                 <button class="restart" @click="restart">
                     Restart
-               </button>
+                </button>
             </div>
         </div>
         <div class="info">
